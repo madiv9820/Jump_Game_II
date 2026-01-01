85e349bf09fdf56489440cf9933f6552afd3dd1a
madiv9820 <themathur901@gmail.com>
2026-01-01 15:12:12 +0530

    ⚙️  Add Cython build script for C++ solution binding 	- Sets up compilation of solution.pyx as a C++ extension 	- Uses Cython and setuptools for cross-language integration 	- Enables Python tests to interact with C++ logic 	- Keeps build minimal and Python 3 compatible

---
+++ source/cpp/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+ext = Extension('solution', sources = ['solution.pyx'], language = 'c++')
+setup(ext_modules = cythonize(ext, language_level = 3))
